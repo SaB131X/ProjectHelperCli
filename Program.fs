6be23b519f3ccb1c03e9aaf8919fd610b82938da
SaB131X <sea1024wm@gmail.com>
2026-09-19 16:57:25 +0700

    initial changes

---
+++ Program.fs
@@ -0,0 +1,12 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+open System.CommandLine
+open Option
+
+[<EntryPoint>]
+let Main(args : string array) =
+    let localProjectName : string = args[0]
+    let gitProjectName : string = args[1]
+    let verboseOption = Option<string>("--verbose", [| "-v" |], Description = "Enable verbose logging")
+    printfn "Hello from F#"
+    0
